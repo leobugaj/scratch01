d078c2956e98885af015e84cdca67fbaba64b0f9
leobugaj <60800182+leobugaj@users.noreply.github.com>
2021-09-16 08:10:17 -0700

    Update madlips.py

--- madlips.py
+++ madlips.py
@@ -1,2 +1,19 @@
-print("Hello World")
-print("Hello fellow")
+print("First I need some information from you.")
+name = input("What is your name: ")
+print("Hi " + name + " How are you. ")
+print("I hope you are Great. ")
+print("Now I need to know little more about you. ")
+age = input("How old are you? ")
+print("wow you are " + age + " years old: ")
+game = input("Would you like to play a GAME: ")
+if game == "yes":
+    print("LETS DO IT")
+else:
+    print("Sorry we will play anyway.")
+
+place = input("What is your favorite place?")
+friend =input("Who is your best friend ")
+
+
+print("We went to the " + place + " to kill some time")
+print(name + " and " + friend + " are covered in a slime")
